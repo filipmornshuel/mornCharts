e5a9d6fc938d77a68ff7e09ae554db3df033a90a
filipmornshuel <filip.slavko.co@gmail.com>
2025-03-16 14:01:18 +0100

    Initial commit

---
+++ src/main/java/ch/morn/historian/charts/mornCharts/MornChartsApplication.java
@@ -0,0 +1,13 @@
+package ch.morn.historian.charts.mornCharts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MornChartsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MornChartsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/morn/historian/charts/mornCharts/MornChartsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.morn.historian.charts.mornCharts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MornChartsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
