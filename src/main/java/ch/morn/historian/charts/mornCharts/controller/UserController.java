50ba4451591aa82710bd0869488afb60259bc6be
filipmornshuel <filip.slavko.co@gmail.com>
2025-03-16 15:18:10 +0100

    Adding hypermedia powered content to the user answers by using HATEOAS

--- src/main/java/ch/morn/historian/charts/mornCharts/controller/UserController.java
+++ src/main/java/ch/morn/historian/charts/mornCharts/controller/UserController.java
@@ -1,37 +1,51 @@
 package ch.morn.historian.charts.mornCharts.controller;
 
+import ch.morn.historian.charts.mornCharts.controller.modelAssembler.UserModelAssembler;
 import ch.morn.historian.charts.mornCharts.exception.UserNotFoundException;
 import ch.morn.historian.charts.mornCharts.model.User;
 import ch.morn.historian.charts.mornCharts.repository.UserRepository;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 public class UserController {
     private final UserRepository repository;
+    private final UserModelAssembler modelAssembler;
 
-    public UserController(UserRepository repository) {
+    public UserController(UserRepository repository, UserModelAssembler modelAssembler) {
         this.repository = repository;
+        this.modelAssembler = modelAssembler;
     }
 
     @GetMapping("/users")
-    List<User> getAllUser() {
-        return repository.findAll();
+    public CollectionModel<EntityModel<User>> getAllUser() {
+        List<EntityModel<User>> users = repository.findAll().stream()
+                .map(modelAssembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(users, linkTo(methodOn(UserController.class).getAllUser()).withSelfRel());
     }
 
     @PostMapping("/users")
-    User saveNewUser(@RequestBody User newUser) {
+    public User saveNewUser(@RequestBody User newUser) {
         return repository.save(newUser);
     }
 
     @GetMapping("/users/{id}")
-    User getOneUser(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    public EntityModel<User> getOneUser(@PathVariable Long id) {
+        User user = repository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+
+        return modelAssembler.toModel(user);
+
     }
 
     @PutMapping("/users/{id}")
-    User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+    public User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
         return repository.findById(id)
                 .map(user -> {
                     user.setUsername(newUser.getUsername());
@@ -47,7 +61,7 @@ public class UserController {
     }
 
     @DeleteMapping("/users/{id}")
-    void deleteUser(@PathVariable Long id) {
+    public void deleteUser(@PathVariable Long id) {
         repository.deleteById(id);
     }
 }
---
+++ src/main/java/ch/morn/historian/charts/mornCharts/controller/modelAssembler/UserModelAssembler.java
@@ -0,0 +1,20 @@
+package ch.morn.historian.charts.mornCharts.controller.modelAssembler;
+
+import ch.morn.historian.charts.mornCharts.controller.UserController;
+import ch.morn.historian.charts.mornCharts.model.User;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.RepresentationModelAssembler;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+public class UserModelAssembler implements RepresentationModelAssembler<User, EntityModel<User>> {
+    @Override
+    public EntityModel<User> toModel(User user) {
+        return EntityModel.of(user,
+                WebMvcLinkBuilder.linkTo(methodOn(UserController.class).getOneUser(user.getId())).withSelfRel(),
+                linkTo(methodOn(UserController.class).getAllUser()).withRel("users"));
+    }
+}
