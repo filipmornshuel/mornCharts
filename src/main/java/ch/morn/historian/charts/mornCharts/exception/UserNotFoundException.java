572389f7183c286b01860fc4813ddabe16abf14a
filipmornshuel <filip.slavko.co@gmail.com>
2025-05-06 22:16:15 +0200

    Fixing User

--- src/main/java/ch/morn/historian/charts/mornCharts/exception/UserNotFoundException.java
+++ src/main/java/ch/morn/historian/charts/mornCharts/exception/UserNotFoundException.java
@@ -1,7 +1,7 @@
 package ch.morn.historian.charts.mornCharts.exception;
 
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(Long id) {
-        super("Could not find user " + id);
+    public UserNotFoundException(String message) {
+        super(message);
     }
 }
